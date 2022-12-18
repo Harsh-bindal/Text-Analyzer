55c851d1a931db0e2c2a8be20a10c97c3723fef2
Harsh bindal <108680668+Harsh-bindal@users.noreply.github.com>
2022-12-18 20:58:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+import Textform from './components/Textform';
+import { useState } from 'react';
+import React from 'react'
+import Alert from './components/Alert';
+// import { BrowserRouter,Switch,Link, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom";
+
+
+
+
+function App() {
+  const [mode, setmode] = useState('light');
+  const  [alert, setalert] = useState(null);
+
+  const ShowAlert =(message,type)=>
+  {
+        setalert
+        (
+          { msg: message,
+              type: type
+
+          }
+        )
+
+        setTimeout(() => {
+          setalert(null);
+        }, 2500);
+  }
+
+  const toggle= ()=>
+  {
+      if(mode==='dark')
+    {
+  setmode('light');
+  document.body.style.backgroundColor='white';
+  ShowAlert("Light mode enabled","success");
+  document.title="TextUtils-Light mode";
+    }
+
+    else{
+      setmode('dark');
+      document.body.style.backgroundColor='grey';
+      ShowAlert("Dark mode enabled","success");
+
+    //  setInterval(() => {
+    //   document.title='TextUtils-Amazing Dark mode';
+    //  }, 1000);
+
+    //  setInterval(() => {
+    //   document.title='TextUtils-Install Dark mode';
+    //  }, 700);
+
+
+    }
+  }
+
+  return (
+ <>
+
+
+
+  <Router>
+<div className="container">
+ <Navbar title="Text Analyzer"  text="About" mode={mode} togglemode={toggle}></Navbar>
+  <Alert  alert={alert} ></Alert>
+  {/* <Textform title="hello" ></Textform> */}
+
+  <Routes>
+      <Route path="/" element={<Textform title="Text Analyzer=word counter,charecter counter"  mode={mode} togglemode={toggle}/>}/>
+      <Route exact path="/About" element={<About></About>}/>
+      <Route exact path="/Home" element={<Home></Home>}/>
+      <Route exact path="/Textform" element={<Textform  />}/>
+      </Routes>
+</div>
+        
+    </Router>  
+
+   {/* <Textform  mode={mode}>   </Textform>
+   <About></About>  */}
+
+ 
+    
+
+    
+
+       
+   
+
+
+
+
+ 
+ </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function () {
+  return (
+
+    
+    <div><h1> 
+         <strong>Welcome to my website!
+            </strong>
+        </h1>
+            </div>
+  )
+}
